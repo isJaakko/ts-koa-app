5580e2116290c6928f4e3d6ca7672ab2cba4a6b9
isJaakko <1512803764@qq.com>
2020-06-21 00:32:47 +0800

    :fire: 删除非模版文件

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
 import Router from '@koa/router';
 import demoRouter from './demo';
-import sketchRouter from './sketch';
 const router = new Router();
 
 router.use(demoRouter.routes());
-router.use(sketchRouter.routes());
 
 export default router;
